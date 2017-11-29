89e0eb11303127bd3f259824a52bd5c0e45553ee
Adam <aelimelech@salesforce.com>
2017-11-29 21:33:18 +0200

    add timepicker

--- IBlockedU/app/src/main/java/com/salesforce/iblockedu/IBlockedUMainActivity.java
+++ IBlockedU/app/src/main/java/com/salesforce/iblockedu/IBlockedUMainActivity.java
@@ -1,11 +1,16 @@
 package com.salesforce.iblockedu;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -19,6 +24,7 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -27,6 +33,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.Calendar;
+import java.util.List;
 //import com.loopj.android.http.JsonHttpResponseHandler;
 //import com.loopj.android.http.RequestParams;
 //
@@ -224,5 +233,42 @@ public class IBlockedUMainActivity extends AppCompatActivity
         fragmentTransaction.replace(R.id.main_content, IBlockedUFormFragment.newInstance(detetction));
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+    }
+
+    public void showTimePickerDialog(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            String newTime = hourOfDay + ":" + minute;
+            List<Fragment> fragmetns = getFragmentManager().getFragments();
+            IBlockedUFormFragment iBlockedUFragment = null;
+            for (Fragment f : fragmetns){
+                if (f  instanceof  IBlockedUFormFragment) {
+                    iBlockedUFragment =(IBlockedUFormFragment) f;
+                    break;
+                }
+            }
+            if (iBlockedUFragment != null){
+                ((TextView)iBlockedUFragment.getActivity().findViewById(R.id.exit_time)).setText(newTime);
+            }
+        }
     }
 }
